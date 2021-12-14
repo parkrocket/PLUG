cbab45be5db0511b5ce3d9621755a34eb4553e9b
hs109 <hc32125@gmail.com>
2021-12-14 18:14:11 +0900

    홍서원

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,39 +1,78 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import socialIcon01 from '../Images/ico_naver.png';
+import socialIcon02 from '../Images/ico_kakao.png';
+import login_bn from '../Images/login_banner.png';
+import '../Css/default.css';
+import './login.css';
+import Head from '../Comp/Head.js';
+import Footer from '../Comp/Footer.js';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-  const [mbId, setMbId] = useState('');
-  const [mbPass, setMbPass] = useState('');
+    const [mbId, setMbId] = useState('');
+    const [mbPass, setMbPass] = useState('');
 
-  const mbIdHandler = (e) => {
-    setMbId(e.target.value);
-  };
-  const mbPassHandler = (e) => {
-    setMbPass(e.target.value);
-  };
+    const mbIdHandler = (e) => {
+        setMbId(e.target.value);
+    };
+    const mbPassHandler = (e) => {
+        setMbPass(e.target.value);
+    };
 
-  const submitHandler = (e) => {
-    e.preventDefault();
+    const submitHandler = (e) => {
+        e.preventDefault();
 
-    let data = { mb_id: mbId, mb_pass: mbPass };
-    axios
-      .post('http://175.125.95.182:4000/login', data)
-      .then((res) => {
-        console.log(res);
-      })
-      .catch();
-  };
+        let data = { mb_id: mbId, mb_pass: mbPass };
+        axios
+        .post('http://175.125.95.182:4000/login', data)
+        .then((res) => {
+            console.log(res);
+        })
+        .catch();
+    };
 
-  return (
-    <div>
-      <form onSubmit={submitHandler}>
-        <h2>{mbId}</h2>
-        <input type="text" name="mb_id" defaultValue="" onChange={mbIdHandler}></input>
-        <input type="password" name="mb_pass" defaultValue="" onChange={mbPassHandler}></input>
-        <input type="submit" defaultValue="로그인"></input>
-      </form>
-    </div>
-  );
+    return (
+        <div>
+            <Head></Head>
+      
+            <form onSubmit={submitHandler}>
+                <div className='body_wrap'>
+                    <p className="logo_title">로그인</p>
+                    <div className='input_wrap'>
+                        <input type="text" name="mb_id" id='' className='login_box box_1 write_box_style' defaultValue="" onChange={mbIdHandler} placeholder='이메일'></input>
+                        <input type="password" name="mb_pass" id='' className='login_box box_2 write_box_style' defaultValue="" onChange={mbPassHandler} placeholder='비밀번호'></input>
+                        <input type="submit" value="로그인" className='login_btn'></input>
+                        <div className="check_email">
+                            <input type="checkbox" name="auto_login" id="login_auto_login" className="checkbox_com"></input>
+                            <label htmlFor="login_auto_login" className="ck_view"></label>
+                            <span htmlFor="login_auto_login" className="email_save">이메일 저장</span>
+                        </div>
+                        <p className="or">
+                            <span className="or_2">또는</span>
+                        </p>
+                        <div className="social_login">
+                            <p>SNS계정으로 간편 로그인/회원가입</p>
+                            <div className="social">
+                                <Link to='#' className='sns-icon social_link sns-naver'><img src={socialIcon01} alt=''/></Link>
+                                <Link to='#' className='sns-icon social_link sns-kakao'><img src={socialIcon02} alt=''/></Link>
+                            </div>
+                            <div className='login_banner'>
+                                <Link to='#'><img src={login_bn} alt=''/></Link>
+                            </div>
+                            <div className="login_etc">
+                                <Link to='#'>이메일로 가입</Link>
+                                <Link to='#'>비밀번호 찾기</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            </form>
+      
+            <Footer></Footer>
+        </div>
+    );
 };
 
 export default Login;
