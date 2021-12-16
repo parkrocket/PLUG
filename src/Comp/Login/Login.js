256be4ed96b2a8b8faeac1c24860c9e096a80d75
parkrocket <mongkys@naver.com>
2021-12-16 10:58:11 +0900

    2021-12-16 플러그

--- src/Comp/Login/Login.js
+++ src/Comp/Login/Login.js
@@ -26,47 +26,6 @@ const Login = () => {
   const submitHandler = (e) => {
     e.preventDefault();
 
-<<<<<<< HEAD
-    return (
-        <div>
-            <Head></Head>
-      
-            <form onSubmit={submitHandler}>
-                <div className='login_wrap'>
-                    <p className='logo_title'>로그인</p>
-                    <div className='input_wrap'>
-                        <input type='text' name='mb_id' id='' className='login_box box_1 write_box_style' defaultValue='' onChange={mbIdHandler} placeholder='이메일'></input>
-                        <input type='password' name='mb_pass' id='' className='login_box box_2 write_box_style' defaultValue='' onChange={mbPassHandler} placeholder='비밀번호'></input>
-                        <input type='submit' value='로그인' className='login_btn'></input>
-                        <div className='check_email'>
-                            <input type='checkbox' name='auto_login' id='login_auto_login' className='checkbox_com'></input>
-                            <label htmlFor='login_auto_login' className='ck_view'></label>
-                            <span htmlFor='login_auto_login' className='email_save'>이메일 저장</span>
-                        </div>
-                        <p className='or'>
-                            <span className='or_2'>또는</span>
-                        </p>
-                        <div className='social_login'>
-                            <p>SNS계정으로 간편 로그인/회원가입</p>
-                            <div className='social'>
-                                <Link to='#' className='sns-icon social_link sns-naver'><img src={socialIcon01} alt=''/></Link>
-                                <Link to='#' className='sns-icon social_link sns-kakao'><img src={socialIcon02} alt=''/></Link>
-                            </div>
-                            <div className='login_banner'>
-                                <Link to='#'><img src={login_bn} alt=''/></Link>
-                            </div>
-                            <div className='login_etc'>
-                                <Link to='#'>이메일로 가입</Link>
-                                <Link to='/pwlost'>비밀번호 찾기</Link>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                
-            </form>
-      
-            <Footer></Footer>
-=======
     let data = { mb_id: mbId, mb_pass: mbPass };
     axios
       .post('http://175.125.95.182:4000/login', data)
@@ -105,6 +64,7 @@ const Login = () => {
             <input
               type="text"
               name="mb_id"
+              id=""
               className="login_box box_1 write_box_style"
               defaultValue=""
               onChange={mbIdHandler}
@@ -113,6 +73,7 @@ const Login = () => {
             <input
               type="password"
               name="mb_pass"
+              id=""
               className="login_box box_2 write_box_style"
               defaultValue=""
               onChange={mbPassHandler}
@@ -151,11 +112,10 @@ const Login = () => {
               </div>
               <div className="login_etc">
                 <Link to="#">이메일로 가입</Link>
-                <Link to="#">비밀번호 찾기</Link>
+                <Link to="/pwlost">비밀번호 찾기</Link>
               </div>
             </div>
           </div>
->>>>>>> 12781f96ab3aa8c81e2d2fc73c54a7d4dc8555b8
         </div>
       </form>
 
